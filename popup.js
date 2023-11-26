053c659c2101aa92911c1d0a3e84ca2c3c57827f
Sanchit <sanchit.n71@rediffmail.com>
2023-11-26 20:10:52 +0530

    updated ui and the backend

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Set placeholder text for the answer-container
+    document.getElementById('answer-container').classList.add('placeholder');
+
     // Add an event listener to the submit button
     document.getElementById('submit-button').addEventListener('click', function () {
         // Get the user's query from the input field
@@ -18,8 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Extract video ID from the YouTube URL
                 var videoId = extractVideoId(currentUrl);
 
-                // Send video ID and user query to the FastAPI backend
-                sendRequestToBackend(videoId, query);
+                // Fetch video title from YouTube page
+                getVideoTitle(videoId, function (title) {
+                    // Update the UI with video title
+                    document.getElementById('video-title').textContent = `Video Title: ${title}`;
+
+                    // Disable the submit button while processing
+                    document.getElementById('submit-button').disabled = true;
+                    document.getElementById('submit-button').innerHTML = 'Processing...';
+
+                    // Send video ID and user query to the FastAPI backend
+                    sendRequestToBackend(videoId, query);
+                });
             } else {
                 document.getElementById('answer-container').textContent = 'This is not a YouTube page.';
             }
@@ -33,8 +46,23 @@ function isYouTubeUrl(url) {
 
 function extractVideoId(url) {
     var match = url.match(/[?&]v=([^&]+)/);
-    console.log(match);
     return match ? match[1] : null;
+}
+
+function getVideoTitle(videoId, callback) {
+    // Fetch video title from YouTube API or extract it from the page
+    // You may need to use a YouTube API key for more accurate results
+    // For simplicity, let's assume the title can be extracted from the page
+    chrome.tabs.executeScript({
+        code: 'document.title',
+    }, function (result) {
+        var pageTitle = result && result[0] ? result[0] : 'Unknown Title';
+
+        // Extract the part before " - YouTube"
+        var cleanTitle = pageTitle.split(' - YouTube')[0];
+
+        callback(cleanTitle.trim());
+    });
 }
 
 function sendRequestToBackend(videoId, query) {
@@ -48,11 +76,20 @@ function sendRequestToBackend(videoId, query) {
     })
         .then(response => response.json())
         .then(data => {
-            // Display the answer from the backend
-            document.getElementById('answer-container').textContent = `Answer: ${data.answer}`;
+            // Fetch video title
+            getVideoTitle(videoId, function (title) {
+                // Display the video title and answer
+                document.getElementById('answer-container').innerHTML = `<strong>Answer:</strong> ${data.answer}`;
+                document.getElementById('answer-container').classList.remove('placeholder');
+            });
         })
         .catch(error => {
             console.error('Error:', error);
             document.getElementById('answer-container').textContent = 'Error communicating with the backend.';
+        })
+        .finally(() => {
+            // Reset button state regardless of success or failure
+            document.getElementById('submit-button').disabled = false;
+            document.getElementById('submit-button').innerHTML = 'Ask??';
         });
 }
